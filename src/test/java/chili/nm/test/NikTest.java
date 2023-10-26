2e47c896a2542f54541728bbd1ad653c5bd04c46
NikolayMkv <i@nikomar.ru>
2023-10-26 19:45:43 +0700

    add delet (#1)
    * add delet
    
    * add delet
    
    * add delet
    
    * add delet
    
    * add delet
    
    * add test
    
    * add test
    
    * add test
    
    * add test
    
    * add test

--- src/test/java/chili/nm/test/NikTest.java
+++ src/test/java/chili/nm/test/NikTest.java
@@ -1,14 +1,21 @@
 package chili.nm.test;
 
-import chili.nm.runner.BaseTest;
+import com.codeborne.selenide.Configuration;
 import org.testng.annotations.Test;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.CollectionCondition.*;
 
 import static com.codeborne.selenide.Selenide.open;
 
-public class NikTest extends BaseTest{
+public class NikTest{
+
     @Test
+
     public void userCanLoginByUsername() {
-//        Configuration.browserSize ="1920x1080";
+        Configuration.browserSize ="1920x1080";
+        Configuration.headless = true;
         open("https://openweathermap.org/");
     }
 }
