02979825dc49936700ef9b83411a2ea0b3d8eb58
Nikolai Markow <i@nikomar.ru>
2023-10-26 19:32:48 +0700

    add test

--- src/test/java/chili/nm/test/NikTest.java
+++ src/test/java/chili/nm/test/NikTest.java
@@ -2,6 +2,10 @@ package chili.nm.test;
 
 import com.codeborne.selenide.Configuration;
 import org.testng.annotations.Test;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.CollectionCondition.*;
 
 import static com.codeborne.selenide.Selenide.open;
 
