a10a229f112a0c7718bb1b35b4b126111197b1dc
NikolayMkv <i@nikomar.ru>
2023-10-31 10:08:52 +0700

    add nm/RF (#7)
    * add nm/RF
    
    * add nm/RF
    
    * add nm/RF

--- src/test/java/chili/nm/test/NikTest.java
+++ src/test/java/chili/nm/test/NikTest.java
@@ -1,17 +1,13 @@
 package chili.nm.test;
 
-import com.codeborne.selenide.Configuration;
+import chili.nm.test.runner.BaseTest;
 import org.testng.annotations.Test;
-
 import static com.codeborne.selenide.Selenide.open;
 
-public class NikTest{
+public class NikTest extends BaseTest {
 
     @Test
-
     public void userCanLoginByUsername() {
-        Configuration.browserSize ="1920x1080";
-        Configuration.headless = true;
         open("https://openweathermap.org/");
     }
 }
